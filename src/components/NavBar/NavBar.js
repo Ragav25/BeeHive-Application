923278de1ad3c47283191498a1360bba71403954
Ragav25 <ragavravi25@gmail.com>
2020-04-30 21:01:20 +0100

    adding back button is completed along with styling

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,12 +1,18 @@
 import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 import "./NavBar.css";
+
 import { ThemeContext } from "../../context/ThemeContext";
 
-const NavBar = () => {
+const NavBar = (props) => {
   const { isLightTheme, light, dark } = useContext(ThemeContext);
   const theme = isLightTheme ? light : dark;
   return (
     <nav id="top-nav" style={{ background: theme.navc, color: theme.textc }}>
+      <Link to="/">
+        <div id="back-button">{props.children}</div>
+      </Link>
+
       <h4 id="logo">BEE HIVE</h4>
     </nav>
   );
--- src/components/projectcomponents/StoriesComponents.js
+++ src/components/projectcomponents/StoriesComponents.js
@@ -1,16 +1,23 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
 import NavBar from "../NavBar/NavBar";
 import ThemeContextProvider from "../../context/ThemeContext";
 import Stories from "../Stories/Stories";
 import StoriesComponentProvider from "../../context/StoriesContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleDoubleLeft } from "@fortawesome/free-solid-svg-icons";
 
 const StoriesComponent = ({ match }) => {
   return (
     <>
       <ThemeContextProvider>
         <StoriesComponentProvider match={match.params}>
-          <NavBar />
+          <NavBar>
+            <Button className="btn-arrow-left">
+              <FontAwesomeIcon icon={faAngleDoubleLeft} />
+            </Button>
+          </NavBar>
           <Stories match={match.params} />
         </StoriesComponentProvider>
       </ThemeContextProvider>
