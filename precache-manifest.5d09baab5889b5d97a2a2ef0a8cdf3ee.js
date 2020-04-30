c3f71357f6312b3eafb0837f3fb1f8ce17eb7471
Ragav25 <ragavravi25@gmail.com>
2020-05-01 00:55:10 +0100

    Updates

---
+++ precache-manifest.5d09baab5889b5d97a2a2ef0a8cdf3ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c7cb29117ec57aab907eb2235fbf417",
+    "url": "/To-Do_Application/index.html"
+  },
+  {
+    "revision": "3c576378e417d2f4f523",
+    "url": "/To-Do_Application/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "3982d1948f7caa98c0bd",
+    "url": "/To-Do_Application/static/css/main.bbf90a6c.chunk.css"
+  },
+  {
+    "revision": "3c576378e417d2f4f523",
+    "url": "/To-Do_Application/static/js/2.cf0cb21f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/To-Do_Application/static/js/2.cf0cb21f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3982d1948f7caa98c0bd",
+    "url": "/To-Do_Application/static/js/main.52568c6e.chunk.js"
+  },
+  {
+    "revision": "e95b4bf2642ad2e86322",
+    "url": "/To-Do_Application/static/js/runtime-main.52f87388.js"
+  }
+]);
